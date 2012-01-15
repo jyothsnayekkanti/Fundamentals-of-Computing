59ae678c5b04fbd42deccc18689b3f8555e09e8d
Jyothsna Annavarapu <jyothsnayekkanti@gmail.com>
2012-01-15 23:20:09 +0545

    home page

--- gen/com/example/R.java
+++ gen/com/example/R.java
@@ -8,12 +8,20 @@
 package com.example;
 
 public final class R {
+    public static final class array {
+        public static final int numbersystem_array=0x7f040000;
+    }
     public static final class attr {
     }
+    public static final class id {
+        public static final int spinner=0x7f050000;
+    }
     public static final class layout {
-        public static final int main=0x7f020000;
+        public static final int home=0x7f020000;
+        public static final int main=0x7f020001;
     }
     public static final class string {
         public static final int app_name=0x7f030000;
+        public static final int numbersystem_prompt=0x7f030001;
     }
 }
--- src/com/example/MyActivity.java
+++ src/com/example/MyActivity.java
@@ -2,6 +2,8 @@ package com.example;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MyActivity extends Activity
 {
@@ -10,6 +12,13 @@ public class MyActivity extends Activity
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.home);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this, R.array.numbersystem_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
     }
 }
